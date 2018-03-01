4051a769a33775bfa7e817c6d4ed5c3739303626
akash11196 <akash11196@gmail.com>
2018-03-01 13:02:36 +0530

    users model leave model added signup page in progress

---
+++ db/migrate/20180227135707_create_leaves.rb
@@ -0,0 +1,12 @@
+class CreateLeaves < ActiveRecord::Migration[5.0]
+  def change
+    create_table :leaves do |t|
+      t.string :reason
+      t.date :leave_date
+      t.references :user, foreign_key: true
+      t.integer :status, default: 1
+
+      t.timestamps
+    end
+  end
+end
